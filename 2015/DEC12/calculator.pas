cd3bf0b057b854b87f67c1a778534aaf5c8632a2
shep <pshepley@getguru.com>
2021-11-29 19:12:02 -0500

    Add more project files

---
+++ 2015/DEC12/calculator.pas
@@ -0,0 +1,19 @@
+unit Calculator;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+var
+ a: Integer;
+
+implementation
+
+initialization
+  a := 5;
+
+end.
+
---
+++ 2015/DEC13/test/dec13/core_test.clj
@@ -0,0 +1,7 @@
+(ns dec13.core-test
+  (:require [clojure.test :refer :all]
+            [dec13.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
