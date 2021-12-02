75e894f01bc5c7ad1f11964e3ea1dd50c9703a2b
Pete Shepley <peter.shepley@gmail.com>
2021-12-02 07:21:52 -0500

    2021 DEC01 part 2

--- 2021/DEC01/index.js
+++ 2021/DEC01/index.js
@@ -1,5 +1,26 @@
 const fs = require('fs');
 
+class Window {
+  constructor() {
+    this.values = [];
+  }
+
+  isReady() {
+    return this.values.length === 3;
+  }
+
+  push(value) {
+    if (this.values.length >= 3) {
+      this.values.shift();
+    }
+    this.values.push(value);
+  }
+
+  sum() {
+    return this.values.reduce((sum, value) => sum + value, 0);
+  }
+}
+
 async function main() {
   // const input = [
   //   199, 200, 208, 210, 200, 207, 240, 269, 260, 263
@@ -7,13 +28,17 @@ async function main() {
   const text = fs.readFileSync('input', {encoding: 'utf-8', flag: 'r'});
   const input = text.split(/\n/).map((item) => parseInt(item));
 
-  let prevItem;
+  const window = new Window();
+  let prevSum;
   let count = 0;
   input.forEach((item) => {
-    if (prevItem) {
-      count = item > prevItem ? count + 1 : count;
+    window.push(item);
+    if (window.isReady()) {
+      if (prevSum && prevSum < window.sum()) {
+        count += 1;
+      }
+      prevSum = window.sum();
     }
-    prevItem = item;
   });
 
   console.log(`Times increased: ${count}`);
